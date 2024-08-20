30df96859d62993f1be3fe217b5e8fe9bddd4855
Szymon Czapiewski <simonczedu@gmail.com>
2024-08-20 11:25:17 +0000

    Functionality: Add option to add tasks with all their data

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialisation
     let sidevnav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidevnav);
+
+    // datepicker init.
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select init.
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
 });
